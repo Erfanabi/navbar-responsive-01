ff8762391592cc51c99e3e01d4fd442758050dff
Erfan Sharafi <erfansharafi60@gmail.com>
2023-08-12 10:38:53 +0330

    v3

--- Project/script.js
+++ Project/script.js
@@ -1,10 +1,11 @@
 const navbar = document.querySelector(".nav-link");
 const humberg = document.querySelector(".humberg");
 
-
 // humberg.onclick = function () {
 // }
 
 humberg.addEventListener("click", function () {
     navbar.classList.toggle("show-hide");
+
+    humberg.classList.toggle("toggle");
 })
